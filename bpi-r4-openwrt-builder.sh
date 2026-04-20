5b88df2937eed3ff9b3fa14536b26f03e00717b3
Kriann <58628033+Kriannn@users.noreply.github.com>
2026-04-20 22:56:45 +0200

    Update bpi-r4-openwrt-builder.sh

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -3,12 +3,6 @@ set -euo pipefail
 
 cd openwrt;
 
-curl -s -L https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/openwrt-toolchain-mediatek-filogic_gcc-14.3.0_musl.Linux-x86_64.tar.zst | tar --zstd -xvf - -C .
-
 ./scripts/feeds update -a && ./scripts/feeds install -a
-
-curl -o .config https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/config.buildinfo
-
-./scripts/ext-toolchain.sh --toolchain openwrt-toolchain-mediatek-filogic_gcc-14.3.0_musl.Linux-x86_64/toolchain-aarch64_cortex-a53_gcc-14.3.0_musl/ --overwrite-config --config "mediatek_filogic_DEVICE_bananapi_bpi-r4"
 
 make -j $(nproc) defconfig download clean world
