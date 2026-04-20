82c8e86e56023c380405f88c3eebe2a53075159e
Kriann <58628033+Kriannn@users.noreply.github.com>
2026-04-20 22:08:06 +0200

    Create get-openwrt-sources.sh

---
+++ get-openwrt-sources.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf openwrt
+
+git config --global user.email "example@example.org"
+git config --global user.name "example"
+
+git clone https://github.com/openwrt/openwrt.git openwrt
+cd openwrt;
+git remote add wifiHotfix https://github.com/danpawlik/openwrt.git
+git fetch wifiHotfix
+git cherry-pick 4235ed7d6d0d66da384bd487e3ed899c3b315efc
+cd ..
