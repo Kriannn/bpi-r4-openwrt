7da79371010b6c95936fe767e79066124a069021
Kriann <58628033+Kriannn@users.noreply.github.com>
2026-04-20 23:07:56 +0200

    Update bpi-r4-openwrt-builder.sh

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -5,4 +5,12 @@ cd openwrt;
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
+curl -sL -o .config https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/config.buildinfo
+
+sed -i 's/^CONFIG_TARGET_ALL_PROFILES=y/# CONFIG_TARGET_ALL_PROFILES is not set/' .config
+sed -i 's/^CONFIG_TARGET_MULTI_PROFILE=y/# CONFIG_TARGET_MULTI_PROFILE is not set/' .config
+sed -i 's/^CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_.*=y/# &/' .config
+sed -i 's/^# \(CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_bananapi_bpi-r4=y\)/\1/' .config
+sed -i 's/^# \(CONFIG_TARGET_DEVICE_PACKAGES_mediatek_filogic_DEVICE_bananapi_bpi-r4=".*"\)/\1/' .config
+
 make -j $(nproc) defconfig download clean world
